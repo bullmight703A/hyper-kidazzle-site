cee6fcd18d82c49f43d87690cc2ce577d50c0639
Robert Hill <roberthill@kidazzle.com>
2026-03-11 09:33:57 -0400

    UI/UX: Add 100% width interactive Google Map embed to Kidazzle homepage

--- kidazzle/kidaazle html/template-parts/kidazzle/home-content.php
+++ kidazzle/kidaazle html/template-parts/kidazzle/home-content.php
@@ -279,3 +279,16 @@
     }
     renderGraph('infants');
 </script>
+
+<!-- INTERACTIVE MAP SECTION -->
+<section class="w-full bg-slate-100 border-t border-slate-200">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d850239.3877969824!2d-85.18731326464843!3d33.68415715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5038148b5cc13%3A0xc6cb70ebefbab2f2!2sKIDazzle%20Child%20Care%20Inc.!5e0!3m2!1sen!2sus!4v1709425812952!5m2!1sen!2sus" 
+        width="100%" 
+        height="450" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade"
+        title="KIDazzle Locations Map">
+    </iframe>
+</section>
